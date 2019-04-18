307b5f94957d7e08c1d0a57e13b5b804bdcd1010
visualstudio <visualstudio@230-PC20>
2019-04-18 14:31:40 -0400

    vhvffv

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,42 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
+
+
         {
+            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+            using System.Collections;
+            using System.IO;
+
+namespace ConsoleApp1
+    {
+        class Program
+        {
+            ArrayList Beowulf;
+
+            static void Main(string[] args)
+            {
+                Program p = new Program();
+                p.Beowulf = new ArrayList();
+                p.NumLetters();
+            }
+            public void NumLetters()
+            {
+                StreamReader reader = new StreamReader("U:/Users/731777/beowolf.txt");
+                string script = reader.ReadToEnd();
+                int num = 0;
+                foreach (char letter in script)
+                { num++; }
+                Console.WriteLine("Total number of letters per word: " + num);
+
+            }
+
         }
+    }
+}
     }
 }
